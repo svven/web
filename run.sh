fa2cb65c6801a8963ff806c36af9a79afd4f9a14
indefinition <jonscottw@gmail.com>
2014-10-20 21:28:34 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-mkdir -p ~/svven/logs
-export GUNC_ACCESS_LOG=~/svven/logs/guncacc.log
-export GUNC_LOG=~/svven/logs/gunc.log
+mkdir -p ~/svven/web/logs
+export GUNC_ACCESS_LOG=~/svven/web/logs/guncacc.log
+export GUNC_LOG=~/svven/web/logs/gunc.log
 
 cd sources
 gunicorn --access-logfile $GUNC_ACCESS_LOG --log-file $GUNC_LOG --log-level debug run:app -b 0.0.0.0:8000 &

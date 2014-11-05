796422d388c2805d8ef6b92cebfeb130e115cd7c
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2014-11-05 17:17:33 +0100

    Changed folder structure

--- web/app.py
+++ web/app.py
@@ -15,31 +15,31 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 
 
 def sum_counter():
-	try:
-		session['counter'] += 1
-	except KeyError:
-		session['counter'] = 1
+    try:
+        session['counter'] += 1
+    except KeyError:
+        session['counter'] = 1
 
 
 @app.route('/')
 def index():
-	if 'username' in session:
-		sum_counter()
-		return 'Logged in as %s' % escape(session['username'])
-	return 'You are not logged in'
+    if 'username' in session:
+        sum_counter()
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		session['username'] = request.form['username']
-		return redirect(url_for('index'))
-	return '''
-		<form action="" method="post">
-			<p><input type=text name=username>
-			<p><input type=submit value=Login>
-		</form>
-		'''
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+        '''
 
 
 @app.route('/clear')

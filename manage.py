be09e8b656c5a963ec7bcb36332b7749c5e2981d
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-01-29 23:10:19 +0100

    Drop most of it for now
    To be added later one by one

--- manage.py
+++ manage.py
@@ -1,36 +1,28 @@
-# -*- coding: utf-8 -*-
-
-from flask_script import Manager
-
+"""
+Web manager.
+"""
 from web.app import create_app
 from web.extensions import db
+
+from flask_script import Manager
 
 
 app = create_app()
 manager = Manager(app)
-app.config['SENTRY_DSN'] = 'http://e2e8239ee0044f7cbe57c2055820cb10:4c55a700f0554e198ce4fcdcc01a66e9@localhost:9000/2'
-# sentry = Sentry(app)
-
 
 @manager.command
 def run():
-    """Run in local machine."""
-
+    "Run app."
     app.run()
 
-
-@manager.command
-def initdb():
-    """Init/reset database."""
-
-    db.drop_all()
-    db.create_all()
-
+# @manager.command
+# def initdb():
+#     "Init db."
+#     # db.drop_all()
+#     db.create_all()
 
 manager.add_option('-c', '--config',
-                   dest="config",
-                   required=False,
-                   help="config file")
+    dest="config", required=False, help="config file")
 
 if __name__ == "__main__":
     manager.run()
--- web/__init__.py
+++ web/__init__.py
@@ -1,2 +1,12 @@
+"""
+Web package.
+"""
 
+# from flask_sqlalchemy import SQLAlchemy
+# db = SQLAlchemy()
 
+# from flask_cache import Cache
+# cache = Cache()
+
+# from flask_login import LoginManager
+# login_manager = LoginManager()

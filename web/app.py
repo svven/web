0f4f0add01476f601569d8c33ebc444a5931a2c4
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-02-01 01:20:12 +0100

    Reader blueprint

--- web/app.py
+++ web/app.py
@@ -4,7 +4,12 @@ Web app factory.
 import config
 from . import init
 
+from .reader import reader
+
+
+import os.path as path
 import pkgutil, importlib
+
 from flask import Flask, render_template
 
 __all__ = ['create_app'] # from app import *
@@ -15,8 +20,8 @@ def create_app(config_updates=None):
     init(config_updates) # delayed init
     from . import db, assets #, mail, security
 
-    name = __name__.split('.')[0]
-    app = Flask(name)
+    package_name = __name__.split('.')[0]
+    app = Flask(package_name)
     app.config.from_object(config)
 
     db.init_app(app)
@@ -24,7 +29,10 @@ def create_app(config_updates=None):
     # mail.init_app(app)
     # security.init_app(app)
 
-    # register_blueprints(app, name, __path__)
+    package_path = path.join(path.dirname(__file__))
+    # register_blueprints(app, package_name, package_path)
+
+    app.register_blueprint(reader)
 
     if not app.debug:
         for e in [403, 404, 500]:
@@ -36,19 +44,18 @@ def handle_error(e):
     return render_template('errors/%s.html' % e.code), e.code
 
 # def register_blueprints(app, package_name, package_path):
-#     """Register all Blueprint instances on the specified Flask application found
-#     in all modules for the specified package.
-
-#     :param app: the Flask application
-#     :param package_name: the package name
-#     :param package_path: the package path
 #     """
+#     Register all Blueprint instances on the specified 
+#     Flask app found in all modules for the specified package.
+#     """
+#     print package_name, package_path
 #     rv = []
 #     for _, name, _ in pkgutil.iter_modules(package_path):
 #         m = importlib.import_module('%s.%s' % (package_name, name))
 #         for item in dir(m):
 #             item = getattr(m, item)
 #             if isinstance(item, Blueprint):
+#                 print item
 #                 app.register_blueprint(item)
 #             rv.append(item)
 #     return rv
---
+++ web/reader/views.py
@@ -0,0 +1,27 @@
+"""
+Reader blueprint.
+"""
+from flask import Blueprint, render_template
+
+from sqlalchemy.orm import joinedload, contains_eager
+
+from database.news import *
+from database.twitter import *
+
+reader = Blueprint('reader', __name__)
+
+@reader.route('/@<screen_name>')
+def marks(screen_name):
+	readers = Reader.query.join(User).\
+		options(contains_eager(Reader.twitter_user)).\
+		order_by(User.screen_name).all() # temporary
+
+	user = User.query.filter_by(screen_name=screen_name).first()
+	if user and user.reader:
+		marks = user.reader.marks.join(Link).\
+			options(contains_eager(Mark.link)).\
+			order_by(Mark.moment.desc()).limit(30)
+	else:
+		marks = []
+	return render_template('reader/marks.html', 
+		user=user, marks=marks, readers=readers)

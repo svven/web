cd53865cc413ce46cd2df5fdfed6cde864f50c72
Alexandru Stanciu <ducu@svven.com>
2015-03-16 20:37:01 +0000

    Order edition and fellows and display values.

--- web/news/views.py
+++ web/news/views.py
@@ -1,7 +1,9 @@
 """
 News blueprint.
 """
+from operator import attrgetter
 from flask import Blueprint, render_template, abort
+from flask.ext.login import login_required
 from sqlalchemy.orm import joinedload, contains_eager
 
 from database.models import *
@@ -9,7 +11,7 @@ from aggregator.mixes import *
 
 news = Blueprint('news', __name__)
 
-@news.route('/@<screen_name>/marks')
+@news.route('/@<screen_name>')
 def marks(screen_name):
     reader = MixedReader.query.\
         outerjoin(TwitterUser).options(
@@ -27,6 +29,7 @@ def marks(screen_name):
         reader=reader, marks=marks)
 
 @news.route('/@<screen_name>/edition')
+@login_required
 def edition(screen_name):
     reader = MixedReader.query.\
         outerjoin(TwitterUser).options(
@@ -36,10 +39,18 @@ def edition(screen_name):
         abort(404)
     
     reader.aggregate() # temp
-    edition = reader.edition.all()
-    fellows = reader.fellows.options(
-        joinedload(MixedReader.auth_user),
-        joinedload(MixedReader.twitter_user)).all()
+    
+    # fellows = reader.fellows.all()
+    fellows_ids = {fid[0]: fid[1] for fid in reader.get_fellows(withscores=True)}
+    fellows = MixedReader.query.filter(MixedReader.id.in_(fellows_ids.keys())).all()
+    for f in fellows: f.fellowship = fellows_ids[str(f.id)]
+    fellows.sort(key=attrgetter('fellowship'), reverse=True)
+    
+    # edition = reader.edition.all()
+    edition_ids = {nid[0]: nid[1] for nid in reader.get_edition(withscores=True)}
+    edition = MixedLink.query.filter(MixedLink.id.in_(edition_ids.keys())).all()
+    for n in edition: n.relevance = edition_ids[str(n.id)]
+    edition.sort(key=attrgetter('relevance'), reverse=True)
     
     for link in edition:
         markers_ids = link.get_markers()

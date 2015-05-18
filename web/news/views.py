0649e213090e4cdd2b9ba02ad675c02db1e730a3
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-05-18 18:12:46 +0300

    Profile page text

--- web/news/views.py
+++ web/news/views.py
@@ -2,7 +2,7 @@
 News blueprint.
 """
 from flask import Blueprint, render_template, abort
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 from sqlalchemy.orm import joinedload, contains_eager
 
 from database.models import *
@@ -15,6 +15,7 @@ import datetime
 timeago = lambda **kvargs:\
     datetime.datetime.utcnow() - datetime.timedelta(**kvargs)
 
+@news.route('/<screen_name>/')
 @news.route('/@<screen_name>/')
 @login_required
 def reader(screen_name):
@@ -34,7 +35,7 @@ def reader(screen_name):
     
     # reader.aggregate() # temp
     reader.set_fellows()
-    reader.set_edition(moment_min=munixtime(timeago(days=1)))
+    reader.set_edition(moment_min=munixtime(timeago(days=3)))
     
     fellows = reader.fellows
     edition = reader.edition
@@ -42,6 +43,9 @@ def reader(screen_name):
     fellows_dict = {str(f.id): f for f in fellows}
     for link in edition:
         link.fellows = [fellows_dict[fid] for fid in link.fellows_ids]
-    
-    return render_template('news/reader.html', 
+
+    ego = current_user.is_authenticated() and \
+        current_user.screen_name == reader.screen_name
+
+    return render_template('news/reader.html', ego=ego, 
         reader=reader, edition=edition, fellows=fellows, marks=marks)

17992b8aba520a4c64e360d55ed0fe4150350fc2
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-08-24 19:51:36 +0300

    Show tweet in popover on link fellow pic

--- web/static/js/popover.js
+++ web/static/js/popover.js
@@ -1,5 +1,5 @@
 // Handle popover context menu
-$('[data-toggle="popover"]').popover({
+$('#menu').popover({
     content: function() {
         var context = $('.context').clone();
         context.removeClass('menu pull-right enable-lg');
@@ -12,8 +12,41 @@ $(function() {
     var item = location.hash;
     var context = $('.context');
     $(context).children().removeClass('active');
-    $(context).find('li:has(>a[href="' + item +'"])').addClass('active');
-    $('[data-toggle="popover"]').popover('hide');
+    $(context).find('li:has(>a[href="' + item +'"])').addClass('active');      
+    $('#menu').popover('hide');
   });
   $(window).hashchange();
+});
+
+// Create Tweets
+$(".status").popover({
+  content: function() {
+    var anchor = $(this)[0];
+    var tweetId = String($(this).data("tweet-id"));
+    var target = document.createElement("div");
+    target.style.width = "245px";
+    twttr.widgets.createTweet(tweetId, target, {
+        width: "250",
+        cards: "hidden", 
+        conversation: "none"
+      })
+    .then(function(el) {
+      var doc = el.contentDocument;
+      doc.querySelector(".EmbeddedTweet").style.border = "0";
+      doc.querySelector(".EmbeddedTweet-tweet").style.padding = "0";
+    });
+    return target;
+  }
+});
+
+// Dismiss popver on outside click
+$('body').on('click', function(e) {
+  $('[data-toggle="popover"]').each(function () {
+    //the 'is' for buttons that trigger popups
+    //the 'has' for icons within a button that triggers a popup
+    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 
+      && $('.popover').has(e.target).length === 0) {
+      $(this).popover('hide');
+    }
+  });
 });

752ca1cc12f290e9743d0a7c9926b56ed5a4df68
Alexandru Stanciu <alexandru.stanciu@gmail.com>
2015-09-06 22:47:25 +0200

    Deal with imgs in the end

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,41 +1,4 @@
 /*
- * Images
- * --------------------------------------------------
-*/
-$('img').one('error', function() { 
-  this.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
-});
-
-/*
- * Replace all SVG images with inline SVG
- */
-$('img.svg').each(function() {
-  var $img = $(this);
-  var imgID = $img.attr('id');
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-
-  $.get(imgURL, function(data) {
-    // Get the SVG tag, ignore the rest
-    var $svg = $(data).find('svg');
-    // Add replaced image's ID to the new SVG
-    if(typeof imgID !== 'undefined') {
-      $svg = $svg.attr('id', imgID);
-    }
-    // Add replaced image's classes to the new SVG
-    if(typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass+' replaced-svg');
-    }
-    // Remove any invalid XML tags as per http://validator.w3.org
-    $svg = $svg.removeAttr('xmlns:a');
-    // Also remove style if any
-    $svg.children().removeAttr('style');
-    // Replace image with new SVG
-    $img.replaceWith($svg);
-  }, 'xml');
-});
-
-/*
  * Menu
  * --------------------------------------------------
 */
@@ -149,4 +112,41 @@ $('body').on('click', function(e) {
       $(this).popover('hide');
     }
   });
+});
+
+/*
+ * Images
+ * --------------------------------------------------
+*/
+$('img').one('error', function() { 
+  this.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
+});
+
+/*
+ * Replace all SVG images with inline SVG
+ */
+$('img.svg').each(function() {
+  var $img = $(this);
+  var imgID = $img.attr('id');
+  var imgClass = $img.attr('class');
+  var imgURL = $img.attr('src');
+
+  $.get(imgURL, function(data) {
+    // Get the SVG tag, ignore the rest
+    var $svg = $(data).find('svg');
+    // Add replaced image's ID to the new SVG
+    if(typeof imgID !== 'undefined') {
+      $svg = $svg.attr('id', imgID);
+    }
+    // Add replaced image's classes to the new SVG
+    if(typeof imgClass !== 'undefined') {
+      $svg = $svg.attr('class', imgClass+' replaced-svg');
+    }
+    // Remove any invalid XML tags as per http://validator.w3.org
+    $svg = $svg.removeAttr('xmlns:a');
+    // Also remove style if any
+    $svg.children().removeAttr('style');
+    // Replace image with new SVG
+    $img.replaceWith($svg);
+  }, 'xml');
 });
